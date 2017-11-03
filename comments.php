00c0cbc81cc739710ec19b0f9abecf0fd01bff9b
Dale Moore <dmoore092@gmail.com>
2017-11-03 14:32:50 -0400

    .

--- comments.php
+++ comments.php
@@ -41,7 +41,7 @@ $templinkpath = "http://localhost:8888/Web%20and%20Mobile%202/individualfinal/We
                 <span class="span">Subject:</span>
                 <input type = "text"
                        id = "subject"
-                       size = "45"
+                       size = "456"
                        maxlength = "45"
                        value="" />
                </p>
