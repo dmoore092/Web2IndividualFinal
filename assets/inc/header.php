161f214cbe2766b74be629e260d3156dec155ca7
Dale Moore <dmoore092@gmail.com>
2017-11-03 10:17:49 -0400

    .

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -4,16 +4,16 @@
     <span id="click-here" onclick="openNav()"><img src="http://serenity.ist.rit.edu/~dwmdis/240/project2/assets/media/icons/hamburger25x21.png"></a></span>
         <nav id="myMobilenav" class="mobilenav">
             <a id="close-nav" href="#" class="closebtn" onclick="closeNav(); closeSubNav()">&times;</a>
-            <div class="sub-navlist">
-                <div><a <?php if($page == 'home'){  echo 'class="active"';}?> href="<?php echo $linkpath ?>index.php">Home</a></div>
+            <p class="sub-navlist">
+                <p><a <?php if($page == 'home'){  echo 'class="active"';}?> href="<?php echo $linkpath ?>index.php">Home</a></p>
                 <a onclick="getId(this); openOrClose();" id="rincon-section" <?php if($page == 'rincon'){  echo 'class="active"';}?> href="#">Rincon</a>
                 <div class="float-container">    
                     <a class="subnav1" href="<?php echo $linkpath ?>rincon/rincon.php">Rincon Home</a>
                     <a class="subnav1" href="<?php echo $linkpath ?>rincon/food.php">Rincon Food</a>
                     <a class="subnav1" href="<?php echo $linkpath ?>rincon/bars.php">Rincon Bars</a>
                 </div>
-            </div><!-- end <div class="sub-navlist"> -->
-            <div class="sub-navlist">
+            </p><!-- end <div class="sub-navlist"> -->
+            <p class="sub-navlist">
                 <a onclick="getId(this); openOrClose();" id="sanjuan-section" <?php if($page == 'sanjuan'){  
                 echo 'class="active"';}?> href="#">San Juan</a>
                 <div class="float-container">
@@ -21,8 +21,8 @@
                     <a class="subnav2" href="<?php echo $linkpath ?>sanjuan/food.php">San Juan Food</a>
                     <a class="subnav2" href="<?php echo $linkpath ?>sanjuan/oldsanjuan.php">Old San Juan</a>
                 </div>
-            </div><!-- END <div class="sub-navlist> -->
-            <div class="sub-navlist">
+            </p><!-- END <div class="sub-navlist> -->
+            <p class="sub-navlist">
                 <a onclick="getId(this); openOrClose();" id="laparguera-section" <?php if($page == 'laparguera'){ 
                 echo 'class="active"';}?> href="#">La Parguera</a>
                 <div class="float-container">
@@ -30,9 +30,9 @@
                     <a class="subnav3" href="<?php echo $linkpath ?>laparguera/boating.php">La Parguera Boating</a>
                     <a class="subnav3" href="<?php echo $linkpath ?>laparguera/scuba.php">La Parguera Scuba</a>
                 </div>
-            </div><!-- END <div class="sub-navlist> -->
-            <a <?php if($page == 'about'){ 
-                echo 'class="active"';}?> href="#">About</a>
+            </p><!-- END <div class="sub-navlist> -->
+            <p><a <?php if($page == 'about'){ echo 'class="active"';}?> href="#">About</a></p>
+            <p><a <?php if($page == 'comments'){ echo 'class="active"';}?> href="#">Comments</a></p>
     </nav>
         <img src="http://serenity.ist.rit.edu/~dwmdis/240/project2/assets/media/images/prpanoheader400.png"
              alt="pano of puerto rico" id="prpano" />
