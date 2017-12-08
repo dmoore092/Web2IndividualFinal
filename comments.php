cf70941ef7cf3084b14957b89bfd28483f190ef1
Dale Moore <dmoore092@gmail.com>
2017-12-08 13:36:39 -0500

    .

--- comments.php
+++ comments.php
@@ -139,8 +139,8 @@ $templinkpath = "http://localhost:8888/Web%20and%20Mobile%202/individualfinal/We
                 $num_rows = mysqli_affected_rows($mysqli);
                 
                 //reset all the fields
-                header("Location: http://serenity.ist.rit.edu/~dwmdis/240/project2/comments.php");
-                exit();
+                echo "<script type='text/javascript'> document.location = 'comments.php'; </script>";
+                
                 if($result && $num_rows > 0){
                     $msg = $_POST["name"] . " was successfully saved.";
                 }
