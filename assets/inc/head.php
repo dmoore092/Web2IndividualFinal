f9ee875607e01a39156fd3f85848e00f63d1a693
Dale Moore <dmoore092@gmail.com>
2017-12-08 14:33:05 -0500

    .

--- assets/inc/head.php
+++ assets/inc/head.php
@@ -5,24 +5,24 @@
     <title><?php echo "$title" ?></title>
     <link type="text/css" href="<?php echo $relpath ?>assets/css/main.css" rel="stylesheet">
     <script src='https://www.google.com/recaptcha/api.js'></script><!-- google recaptcha -->
-    <link rel="shortcut icon" href="<?php echo $relpath ?>assets/media/images/icons/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $relpath ?>assets/media/images/icons/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/images/icons/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/images/icons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/images/icons/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/images/icons/android-chrome-192x192.png" sizes="192x192">
-    <meta name="msapplication-square70x70logo" content="<?php echo $relpath ?>assets/media/images/icons/smalltile.png" />
-    <meta name="msapplication-square150x150logo" content="<?php echo $relpath ?>assets/media/images/icons/mediumtile.png" />
-    <meta name="msapplication-wide310x150logo" content="<?php echo $relpath ?>assets/media/images/icons/widetile.png" />
-    <meta name="msapplication-square310x310logo" content="<?php echo $relpath ?>assets/media/images/icons/largetile.png" />
+    <link rel="shortcut icon" href="<?php echo $relpath ?>assets/media/icons/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $relpath ?>assets/media/icons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/icons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/icons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="<?php echo $relpath ?>assets/media/icons/android-chrome-192x192.png" sizes="192x192">
+    <meta name="msapplication-square70x70logo" content="<?php echo $relpath ?>assets/media/icons/smalltile.png" />
+    <meta name="msapplication-square150x150logo" content="<?php echo $relpath ?>assets/media/icons/mediumtile.png" />
+    <meta name="msapplication-wide310x150logo" content="<?php echo $relpath ?>assets/media/icons/widetile.png" />
+    <meta name="msapplication-square310x310logo" content="<?php echo $relpath ?>assets/media/icons/largetile.png" />
 </head>
 <body>
     <main>
